3c2b7eb626b0901f738c9db3242ec86c4f8ab4f3
Jaewon Jung <jaewon.j.1@gmail.com>
2022-12-18 02:46:46 -0500

    huge front end changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,7 @@ import App from './App';
 import { StoreProvider } from './Store';
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- frontend/src/pages/HomeScreen.js
+++ frontend/src/pages/HomeScreen.js
@@ -5,7 +5,8 @@ import Product from "../components/Product";
 import styled from 'styled-components'
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox"
-import background from '../images/new.jpg';
+import background from '../images/background.jpg';
+import backgroundsm from '../images/background-sm.jpg';
 const Styles = styled.div`
     display: flex;
     flex-direction: column;
@@ -29,15 +30,108 @@ const Styles = styled.div`
     img {
         width: 120%;
     }
-    .background {
+    .background-lg {
         display: flex;
         flex-direction: column;
         align-items: center;
         position:relative
+        object-fit: contain;
+        width: 100%;
+        height: 100%;
+        border-radius: 1.2%
+        margin-bottom: 50px;
+    }
+    .background-sm {
+        display: none;
+        flex-direction: column;
+        align-items: center;
+        position:relative
+        object-fit: contain;
+        width: 100%;
+        height: 20%
+        border-radius: 1.2%;
     }
     h1 span {
         color: cyan;
     }
+    .container-lg {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        position:relative;
+    }
+    .container-sm {
+        display: none;
+        flex-direction: column;
+        align-items: center;
+        position:relative
+    }
+    @media screen and (max-width: 500px) {
+        .container-lg{
+           display:none;
+        }
+    
+        .container-sm{
+           display:flex;
+        }
+
+        .background-lg {
+            display: none;
+        }
+
+        .background-sm {
+            display: flex;
+        }
+    }
+    #btn {
+        background: cyan;
+        color: black;
+        height: 50px;
+        min-width: 120px;
+        border: none;
+        border-radius: 10px;
+        position: relative;
+        transition: 1s;
+        -webkit-tap-highlight-color: transparent;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+        padding-top: 5px;
+      }
+      
+      #btn #circle {
+        width: 5px;
+        height: 5px;
+        background: transparent;
+        border-radius: 50%;
+        position: absolute;
+        top: 0;
+        left: 50%;
+        overflow: hidden;
+        transition: 200ms;
+      }
+      
+      .noselect {
+        -webkit-touch-callout: none;
+          -webkit-user-select: none;
+           -khtml-user-select: none;
+             -moz-user-select: none;
+              -ms-user-select: none;
+                  user-select: none;
+      }
+      
+      #btn:hover {
+        background: transparent;
+      }
+      
+      #btn:hover #circle {
+        height: 50px;
+        width: 20vw;
+        left: 0;
+        border-radius: 0;
+        border-bottom: 2px solid #eee;
+      }
 `;
 
 const reducer = (state, action) => {
@@ -77,10 +171,24 @@ function HomePage() {
     return (
 		<Styles>
             <Styles>
-                <img src = {background} className = "background"/>
-                <h1 style = {{position: "absolute", top: "65%", color: "white", fontSize: "65px"}}>Buy and <span>Sell</span> Your Electronics</h1>
+                <div className = "container-lg">
+                    <img src = {background} className = "background-lg"/>
+                    <div style = {{position: "absolute", top: "35%", justifyContent: "center"}}>
+                        <h1 style = {{color: "white", fontSize: "4.5vw", maxWidth:"950px", marginBottom: "2vw"}}>Buy and <span>Sell</span> Your Electronics</h1>
+                        <div style = {{maxWidth: "20vw"}}id="btn"><span class="noselect">Shop Now</span><div id="circle"></div></div>
+                        <div style = {{maxWidth: "20vw", marginTop: "2vw", backgroundColor: "#e6ffff"}}id="btn"><span class="noselect">Sell With Us</span><div id="circle"></div></div>
+                    </div>
+                </div>
+                <div className = "container-sm">
+                    <img src = {backgroundsm} className = "background-sm"/>
+                    <h1 style = {{position: "absolute", top: "12%", color: "black", fontSize: "10vw", maxWidth: "75vw"}}>Buy and <span>Sell </span>Your Electronics</h1>
+                    <div style = {{position: "absolute", top: "76%", justifyContent: "center"}}>
+                        <div style = {{maxWidth: "20vw"}}id="btn"><span class="noselect">Shop Now</span><div id="circle"></div></div>
+                        <div style = {{maxWidth: "20vw", marginTop: "2vw", backgroundColor: "#e6ffff"}}id="btn"><span class="noselect">Sell With Us</span><div id="circle"></div></div>
+                    </div>
+                </div>
             </Styles>
-			<h2>Featured Products</h2>
+			<h2 style = {{marginTop: "100px"}}>Featured Products</h2>
             {loading ? (
                 <LoadingBox />
             ) : error ? (
