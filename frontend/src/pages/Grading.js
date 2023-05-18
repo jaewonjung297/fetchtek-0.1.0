9a1f5da6506c4ce44b7f927e165a33cc5a408b59
mac <mac@macs-MacBook-Pro-2.local>
2023-05-17 22:36:52 -0400

    made changes to grading

--- frontend/src/pages/Grading.js
+++ frontend/src/pages/Grading.js
@@ -62,7 +62,7 @@ export default function Grading() {
                     With FetchTek, you are guarnteed quality. 
                     Our experts thoroughly inspect each product, 
                     evaluating its physical condition, functionality, 
-                    and overall quality. These grading standards will determine 
+                    and overall quality. These grading standards determine 
                     the value of your electronics.
                 </p>
                 <div className='button_small'>
@@ -78,16 +78,51 @@ export default function Grading() {
         </div>
         <div className = 'grading-container'>
             <div className = 'grade'>
-                <p className = 'grade-grade'>A</p>
-                <p className = 'grade-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Praesent sagittis risus nisl, vel viverra purus facilisis in. 
-                    Quisque aliquam, est mattis placerat finibus, sem eros luctus nunc, 
-                    vitae efficitur nulla arcu quis ante. Etiam placerat congue augue non 
-                    feugiat. Cras sed dolor a eros scelerisque posuere.
+                <p className = 'grade-grade'>New: Sealed</p>
+                <p className = 'grade-text'>
+                <ul> 
+                <li>100% brand new phone sealed in the manufacturer's original packaging</li>
+                <li>Device has never been activated or used</li>
+                <li>Original accessories have never been used or tampered with</li>
+                <li>Seal hasn't been tampered with and the box is in perfect condition</li>
+                </ul>
+                </p>
+            </div>
+        </div>
+            <div className = 'grade'>
+                <p className = 'grade-grade'>New: Open box</p>
+                <p className = 'grade-text'>
+                <ul> 
+                <li>These devices must have been activated for less than 60 days</li>
+                <li>Absolutely no scratches or blemishes on any part of the phone</li>
+                <li>All accessories have never been used in the original packaging</li>
+                <li>Device must have its original plastic on the screen</li>
+                </ul>
                 </p>
             </div>
             <div className = 'grade'>
-                <p className = 'grade-grade'>B</p>
+                <p className = 'grade-grade'>A</p>
+                <p className = 'grade-text'>
+                <ul> 
+                <li>Absolutely no scratches on front and back glass</li>
+                <li>Device must pass all functionality tests</li>
+                <li>Maximum of 1 small light scratch (under 1 cm) on housing that can only be uncovered by heavy inspection under a light</li>
+                </ul>
+                </p>
+            </div>
+            <div className = 'grade'>
+                <p className = 'grade-grade'>B1</p>
+                <p className = 'grade-text'>
+                <ul> 
+                <li>Absolutely no scratches on front glass</li>
+                <li>1-3 light scratches on the back glass are permissable </li>
+                <li>Device must pass all functionality tests</li>
+                <li>Maximum of 1 small light scratch on housing that can only be uncovered by heavy inspection under a light</li>
+                </ul>
+                </p>
+            </div>
+            <div className = 'grade'>
+                <p className = 'grade-grade'>B2</p>
                 <p className = 'grade-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                     Praesent sagittis risus nisl, vel viverra purus facilisis in. 
                     Quisque aliquam, est mattis placerat finibus, sem eros luctus nunc, 
@@ -104,8 +139,15 @@ export default function Grading() {
                     feugiat. Cras sed dolor a eros scelerisque posuere.
                 </p>
             </div>
-
-        </div>
+            <div className = 'grade'>
+                <p className = 'grade-grade'>D</p>
+                <p className = 'grade-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Praesent sagittis risus nisl, vel viverra purus facilisis in. 
+                    Quisque aliquam, est mattis placerat finibus, sem eros luctus nunc, 
+                    vitae efficitur nulla arcu quis ante. Etiam placerat congue augue non 
+                    feugiat. Cras sed dolor a eros scelerisque posuere.
+                </p>
+            </div>
     </Styles>
   )
 }
